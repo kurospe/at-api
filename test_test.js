ff2d9f205a326a92982bf6a9885d67ce1149b5a9
kurospe <akuntestspe@gmail.com>
2020-10-05 15:41:55 +0700

    add test login

---
+++ test_test.js
@@ -0,0 +1,8 @@
+Feature('test');
+
+Scenario('test something', ({ I }) => {
+    I.fillField('Masukkan Email / No Telepon Anda', 'akuntestspe+atemk1@gmail.com');
+    I.fillField('Masukan Password Anda', 'Test@123');
+    I.tap('MASUK');
+    I.see('Pilih Outlet');
+});
